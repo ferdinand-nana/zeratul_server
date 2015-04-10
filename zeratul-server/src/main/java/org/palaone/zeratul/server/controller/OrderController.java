926d5302aff91c42718887c4b2743628a5ee8cd6
fnana <ferdinand.nana@nokia.com>
2015-04-10 18:09:37 +0800

    Updated OrderController

--- zeratul-server/src/main/java/org/palaone/zeratul/server/controller/OrderController.java
+++ zeratul-server/src/main/java/org/palaone/zeratul/server/controller/OrderController.java
@@ -27,10 +27,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class OrderController {
 
+	private static final double coordinates[][] = {{121.05774, 14.58318}, {121.03758, 14.6386}, {121.05977, 14.5856}, {121.05485, 14.58036}, {121.03037, 14.6553}};
 	private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 	private static AtomicLong count = new AtomicLong(0);
-	
-	
+
 	@RequestMapping("/order/find/{longitude}/{latitude}")
 	@ResponseBody
 	@Transactional(readOnly = true)
@@ -56,32 +56,34 @@ public class OrderController {
 		orderVO.setTime(new Date());
 		
 		List<BidVO> bidVOs = new ArrayList<BidVO>();
-		bidVOs.add(createBidVO());
-		bidVOs.add(createBidVO());
-		bidVOs.add(createBidVO());
-		bidVOs.add(createBidVO());
-		bidVOs.add(createBidVO());
+		bidVOs.add(createBidVO(0));
+		bidVOs.add(createBidVO(1));
+		bidVOs.add(createBidVO(2));
+		bidVOs.add(createBidVO(3));
+		bidVOs.add(createBidVO(4));
 		orderVO.setBids(bidVOs);
 		
 		return orderVO;
 	}
 	
-	private BidVO createBidVO() {
+	private BidVO createBidVO(int index) {
 		long counter = count.incrementAndGet();
 		
 		BidVO bidVO = new BidVO();
 		bidVO.setAmount(new BigDecimal(counter));
 		bidVO.setTime(new Date());
-		bidVO.setUser(createUserVO());
+		bidVO.setUser(createUserVO(index));
 		return bidVO;
 	}
 	
-	private UserVO createUserVO() {
+	private UserVO createUserVO(int index) {
 		long counter = count.incrementAndGet();
 		
 		UserVO userVO = new UserVO();
 		userVO.setUsername("user" + counter);
 		userVO.setRating(5);
+		userVO.setLongitude(coordinates[index][0]);
+		userVO.setLatitude(coordinates[index][1]);
 		return userVO;
 	}
 }
